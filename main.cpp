5c3ddfe2f66cd9070b2ee05303a1a23f7ecca799
Jason <jkasari@outlook.com>
2020-12-24 19:36:28 -0800

    test diceRoll in terminal

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include "functions.cpp"
+#include <ctime>
 
 using namespace std;
 
 int main() {
+  srand(time(0));
 
-uint32_t sideNum = 6;
-uint32_t diceNum = 3;
-uint32_t* outcomes = posOutcomes(sideNum, diceNum);
+      uint32_t sideNum = 6;
+  uint32_t diceNum = 3;
+  uint32_t* outcomes = posOutcomes(sideNum, diceNum);
 
-for(int i = 0; i < ((sideNum * diceNum) - (diceNum - 1)); ++i) {
-  cout << outcomes[i] << endl;
-}
+  // for(int i = 0; i < ((sideNum * diceNum) - (diceNum - 1)); ++i) {
+  //  cout << outcomes[i] << endl;
+  //}
+
+  uint32_t rollValue = diceRoll(sideNum, diceNum);
+  cout << rollValue << endl;
+
   return 0;
 }
