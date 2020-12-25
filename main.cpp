24791a6d9b1493b1cd277ef1c70210169bfa111a
Jason <jkasari@outlook.com>
2020-12-24 19:04:29 -0800

    test posOutcomes in terminal

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include "functions.cpp"
 
 using namespace std;
 
 int main() {
 
-  cout << "Hello world\n";
+uint32_t sideNum = 6;
+uint32_t diceNum = 3;
+uint32_t* outcomes = posOutcomes(sideNum, diceNum);
 
+for(int i = 0; i < ((sideNum * diceNum) - (diceNum - 1)); ++i) {
+  cout << outcomes[i] << endl;
+}
   return 0;
 }
