36bff22047564f04cb25b064e1c1683b97e462ed
Jason <jkasari@outlook.com>
2020-12-24 20:20:22 -0800

    finish and test diceCalc

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
   srand(time(0));
 
-  uint32_t rollNum = 100;
+  uint32_t rollNum = 1000;
   uint32_t sideNum = 6;
   uint32_t diceNum = 2;
   uint32_t* outcomes = posOutcomes(sideNum, diceNum);
@@ -16,12 +16,13 @@ int main() {
   //  cout << outcomes[i] << endl;
   //}
 
-  //uint32_t rollValue = diceRoll(sideNum, diceNum);
-  //cut << rollValue << endl;
+  // uint32_t rollValue = diceRoll(sideNum, diceNum);
+  // cut << rollValue << endl;
 
   uint32_t* results = diceCalc(sideNum, diceNum, rollNum);
-  for(int i = 0; i < ((sideNum * diceNum) - (diceNum - 1)); ++i) {
-    cout << results[i] << endl;
+  for (int i = 0; i < ((sideNum * diceNum) - (diceNum - 1)); ++i) {
+    cout << static_cast<double>(results[i]) / rollNum * 100 << "------"
+         << outcomes[i] << endl;
   }
   return 0;
 }
