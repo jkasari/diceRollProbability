3434bea1ee0ee3755bebe26baae37b3f3d234f6d
Jason <jkasari@outlook.com>
2020-12-24 19:04:05 -0800

    create posOutcomes function

--- functions.cpp
+++ functions.cpp
@@ -1,26 +1,24 @@
 #include <random>
 #include <iostream>
 #include <algorithm>
+#include <cmath>
 
 /**
- * behives like a die, use the .roll function to generate a virtual roll.
+ * takes an int for the number of sides on a die plus the number of dice and
+ * gives you all possible number outcomes
  */
-class dice {
- public:
-  dice(const uint32_t sides) { this->sides = sides; }
-  uint32_t roll() const { return (rand() % sides) + 1; }
-
- private:
-  uint32_t sides;
-};
+uint32_t* posOutcomes(uint32_t sideNum, uint32_t diceNum) {
+  uint32_t* outcomes = new uint32_t[((sideNum * diceNum) - (diceNum - 1))];
+  uint32_t targetNum = diceNum;
+  for(int i = 0; i < ((sideNum * diceNum) - (diceNum - 1)); ++i) {
+      outcomes[i] = targetNum;
+      targetNum++;
+  }
+  return outcomes;
+}
 
 /**
- * takes an int for the number of sides on a die plus the number of dice and gives you all possible number outcomes
+ * takes an int for the number of sides on a die plus the number of dice and
+ * returns a int* with the percentage for each possible number combo.
  */
-int* posOutcomes(uint32_t sideNum, uint32_t diceNUm) {}
-
-/**
- * takes an int for the number of sides on a die plus the number of dice and returns a int* with the percentage for each possible number combo.
- */
-int* diceCalc(uint32_t sideNum, uint32_t diceNum, &int* outcomes) {}
-
+int* diceCalc(uint32_t sideNum, uint32_t diceNum, int* outcomes) {}
